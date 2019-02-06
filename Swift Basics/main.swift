39bd8952bdbc6923affa457f66b329166188926c
Brady Webb <Bradywebb@Bradys-MacBook-Pro-2.local>
2019-02-05 18:20:21 -0600

    First commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Brady Webb on 2/5/19.
+//  Copyright © 2019 Brady Webb. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
